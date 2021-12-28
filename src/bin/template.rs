20d4d1a3bcdb0e11dc681976e029c43126cb0213
Finomnis <finomnis@gmail.com>
2021-12-28 14:45:39 +0100

    Initial commit

---
+++ src/bin/template.rs
@@ -0,0 +1,24 @@
+use advent_of_code_helpers::*;
+
+fn parse_input(input: &str) -> Result<&str> {
+    Ok(input)
+}
+
+fn task1(_input_data: &str) -> Result<u32> {
+    Ok(0)
+}
+
+fn task2(_input_data: &str) -> Result<u32> {
+    Ok(0)
+}
+
+aoc_tests! {
+    // task1: {
+    //     simple => 0,
+    //     complex => 0,
+    // },
+    // task2: {
+    //     simple => 0,
+    //     complex => 0,
+    // }
+}
