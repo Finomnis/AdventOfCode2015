9c7fffcd984be68256bb247da86b3cd0c3359759
Finomnis <finomnis@gmail.com>
2021-12-28 14:49:58 +0100

    Add day01 inputs

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -4,17 +4,17 @@ fn parse_input(input: &str) -> Result<&str> {
     Ok(input)
 }
 
-fn task1(_input_data: &str) -> Result<u64> {
+fn task1(_input_data: &str) -> Result<i32> {
     Ok(0)
 }
 
-fn task2(_input_data: &str) -> Result<u32> {
+fn task2(_input_data: &str) -> Result<i32> {
     Ok(0)
 }
 
 aoc_tests! {
     task1: {
-        simple => 0,
+        simple => -3,
         complex => 0,
     },
     task2: {
